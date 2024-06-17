7363ccf77f41f6027ad80b9297526397774c230c
fexell <fexellofficial@gmail.com>
2024-06-17 11:24:07 +0200

    Removed all methods from Parameters.php to rework it later

--- src/Http/Request/Parameters/Parameters.php
+++ src/Http/Request/Parameters/Parameters.php
@@ -25,39 +25,4 @@
       $this->response = new Response();
       $this->param = $param;
     }
-
-    public function get() {
-      $result = [];
-
-      if(is_string($this->param))
-        if(empty($_GET[ $this->param ]))
-          exit('Could not find a value for key "' . $this->param . '."');
-
-        else
-          return $_GET[ $this->param ];
-
-      else if(is_array($this->param)) {
-        foreach($this->param as $param)
-          if(empty($_GET[ $param ]))
-            return $this->response->error('Could not find a value for parameter "' . $param . '."')->print();
-
-          else
-            $result[ $param ] = $_GET[ $param ];
-
-        return $this->response->json($result);
-      }
-    }
-
-    public function toJson() {
-      if(is_string($this->get()))
-        return $this->response->json([ $this->param => $this->get() ]);
-
-      else if(is_array($this->get()))
-        return $this->get();
-    }
-
-    public function all() {
-      if(is_null($this->param))
-        return $this->response->json($_GET);
-    }
   }
